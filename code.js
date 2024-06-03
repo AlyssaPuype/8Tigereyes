8ee89fe773daaa7fe381bad9ecf7d268cc658cdb
Alyayeex <alyssa.puype@gmail.com>
2024-06-04 01:05:14 +0200

    tw

--- code.js
+++ code.js
@@ -1,4 +1,20 @@
-function toggleDiv(divId) {
+let i = 0;
+let txt = "Hey, nice to meet you! Let's create amazing websites together! ✨"; 
+let speed = 60; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("twtext").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = function() {
+    typeWriter();
+  };
+
+  function toggleDiv(divId) {
     var div = document.getElementById(divId);
     div.classList.toggle('hidden');
 }
